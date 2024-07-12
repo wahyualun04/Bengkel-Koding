ae2dbe7eb86b55b650d4a459e16058b40d8c9da5
wahyualun04 <155615270+wahyualun04@users.noreply.github.com>
2024-07-12 13:09:21 +0700

    Delete Poliklinik_UAS directory

--- Poliklinik_UAS/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$databaseHost = 'localhost';
-$databaseName = 'poli_uas';
-$databaseUsername = 'root';
-$databasePassword = '';
- 
-$mysqli = mysqli_connect($databaseHost, 
-    $databaseUsername, $databasePassword, $databaseName);
-?>
